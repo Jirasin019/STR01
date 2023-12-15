c45e77387bbd2b7fadd15bba0a870d67607fbb8d
Jirasin64-38 <s14lapg@gmail.com>
2023-12-15 14:21:27 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.header("Jirasin Keadjongrak")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('นายจิรสิน เกิดจงรักษ์')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
